9f9beb1333eab27b1200a05d0b0033850b1afaef
Cody Reynolds <codyreynolds@hey.com>
2021-05-26 23:02:50 -0400

    update data.json

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const data = require('./data.json');
 //Sets the view engine to pug
 app.set('view engine', 'pug');
 
+//Serves the static files - tells the app they are stored in a folder called 'public'
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
+    projects = data.projects;
+    thumbnail = data.projects.image_urls;
     res.render('index');
 });
 
