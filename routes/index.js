bbabede37e6ac9d6d87fea9c455e8ef432e90c1d
Cody Reynolds <codyreynolds@hey.com>
2021-05-28 14:35:36 -0400

    Add 404 error handler

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = require('../app');
 const router = express.Router();
 const {projects} = require('../data/data.json');
 
@@ -11,15 +12,40 @@ router.get('/about', (req, res) => {
     res.render('about', projects);
 });
 
-router.get('/projects/:id', (req, res) => {
+router.get('/projects/:id', (req, res, next) => {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId );
 
     if (project) {
-        res.render('project', { project });
+        return res.render('project', { project });
       } else {
-        res.send('<h1>Not found</h1>');
-      }
+            const err = new Error();
+            err.status = 404;
+            err.message = `The project you requested doesn't exist (yet)!`;
+            next(err);
+        }
+});
+
+
+//404 Error Handler
+router.use((req, res, next) => {
+     console.log('404 error handler called')
+     const err = new Error();
+     err.status = 404;
+     err.message = "This page does not exist.";
+     console.log(err.message);
+     console.log(err.status);
+     res.render('page-not-found', {err} );
+ });
+
+//Global Error Handler
+router.use((err, req, res, next) => {
+    if(err.status === 404) {
+        res.status(404).render('page-not-found', {err});
+    } else {
+        console.log("Something broke!")
+    }
+
 });
 
 module.exports = router;
